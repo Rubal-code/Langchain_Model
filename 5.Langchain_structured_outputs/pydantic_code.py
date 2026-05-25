6540c791f2dfe3059758dfd9e97670b90a2fd392
Rubal-code <rubal3232@gmail.com>
2026-05-25 21:30:06 +0530

    pydantic usage basic and default values

---
+++ 5.Langchain_structured_outputs/pydantic_code.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel #pydantic is the library that allows us to create data models with type validation and parsing. It is commonly used in Python for data validation and settings management.
+
+# basic example
+class my_student(BaseModel):
+    name: str
+    age: int
+    grade: str
+
+new_student={'name': 'Alice', 'age': 20, 'grade': 'A'}
+student1 = my_student(**new_student) # we use ** to unpack the dictionary and pass its contents as keyword arguments to the my_student model. This allows us to create an instance of my_student with the values from the new_student dictionary.
+
+print(student1)
+
+# default values
+class my_teacher(BaseModel):
+    name: str
+    subject: str
+    years_of_experience: int = 5 # we can set a default value for years_of_experience, so if it is not provided when creating an instance of my_teacher, it will default to 5.
+
+teacher1={'name': 'Mr. Smith', 'subject': 'Math'}
+teacher_instance = my_teacher(**teacher1)
+print(teacher_instance.years_of_experience) # this will print 5, since we did not provide a value for years_of_experience when creating the teacher_instance.
